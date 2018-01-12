e9f342f49dc8a0ef59f697d4da1e321d3db67f13
matuteiglesias <matuteiglesias@gmail.com>
2018-01-12 14:02:23 +0100

    added sitecustomize.py for encoding issues

---
+++ source/sitecustomize.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+# necessary for our docstrings containing unicode characters
+# print(100*'#')
+import sys
+if sys.version_info[0]==2:
+    sys.setdefaultencoding('utf-8')
